ae4ef39ca8ff05ac94cefb441a115d9534687974
Shehan Prasanna <shehanp068@gmail.com>
2021-02-03 14:17:15 +0530

    counter

--- src/index.js
+++ src/index.js
@@ -4,45 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import  {createStore} from "redux";
+import allReducers from './reducers'
 
-//store => global State
+const store = createStore(allReducers);
 
-//action
-// const increment = () => {
-//     return {
-//         type:'INCREMENT'
-//     }
-// }
-//
-// const decrement = () => {
-//     return {
-//         type:'DECREMENT'
-//     }
-// }
-//
-// //reducer
-// const counter = (state = 0 , action) => {
-//     switch  (action.type){
-//         case 'INCREMENT':
-//             return state + 1;
-//         case 'DECREMENT':
-//             return  state -1;
-//
-//     }
-//
-// }
-//
-// let store = createStore(counter);
-//
-// //Display in the console
-// store.subscribe(() => console.log(store.getState()));
-//
-//
-// //dispatch
-// store.dispatch(increment());
-// store.dispatch(decrement());
-// store.dispatch(decrement());
-//
 
 
 ReactDOM.render(
--- src/reducers/isLogged.js
+++ src/reducers/isLogged.js
@@ -3,6 +3,8 @@ const loggedReducer = (state = false,action) => {
     switch (action.type){
         case 'SIGN_IN':
             return !state;
+        default:
+            return state;
 
     }
 
