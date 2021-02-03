8cd8aa548783d489b9ce40d417a855acf7959683
Shehan Prasanna <shehanp068@gmail.com>
2021-02-03 11:30:39 +0530

    reducers and actions

--- src/index.js
+++ src/index.js
@@ -8,41 +8,41 @@ import  {createStore} from "redux";
 //store => global State
 
 //action
-const increment = () => {
-    return {
-        type:'INCREMENT'
-    }
-}
-
-const decrement = () => {
-    return {
-        type:'DECREMENT'
-    }
-}
-
-//reducer
-const counter = (state = 0 , action) => {
-    switch  (action.type){
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return  state -1;
-
-    }
-
-}
-
-let store = createStore(counter);
-
-//Display in the console
-store.subscribe(() => console.log(store.getState()));
-
-
-//dispatch
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-
+// const increment = () => {
+//     return {
+//         type:'INCREMENT'
+//     }
+// }
+//
+// const decrement = () => {
+//     return {
+//         type:'DECREMENT'
+//     }
+// }
+//
+// //reducer
+// const counter = (state = 0 , action) => {
+//     switch  (action.type){
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return  state -1;
+//
+//     }
+//
+// }
+//
+// let store = createStore(counter);
+//
+// //Display in the console
+// store.subscribe(() => console.log(store.getState()));
+//
+//
+// //dispatch
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+//
 
 
 ReactDOM.render(
